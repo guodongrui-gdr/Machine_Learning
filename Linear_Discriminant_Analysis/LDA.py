3e834195fe1a465f2a0c00300ca3215e33958081
makabaka003 <3084608158@qq.com>
2022-03-19 02:49:37 +0800

    Linear Discriminant Analysis 交叉验证 45组数平均准确率

--- Linear_Discriminant_Analysis/LDA.py
+++ Linear_Discriminant_Analysis/LDA.py
@@ -1,3 +1,5 @@
+import time
+
 import numpy as np
 from sklearn import preprocessing
 from sklearn.datasets import load_digits  # 导入数据
@@ -18,42 +20,46 @@ def test(x, w, w0):
     flag = y > w0
     return flag
 
-def get_data():
+
+def run(label0, label1):
+    correct_rate = 0
+
     digits = load_digits()
     data = digits.data
     target = digits.target
-# choose train sample:0--999
-    train_x = data[0:1000]
-    train_y = target[0:1000]
 
-    test_x = data[1000:]
-    test_y = target[1000:]
-# 选取两个数字进行分类
-    label0 = 0
-    label1 = 9
-    train_x0 = train_x[train_y == label0, :]
-    train_x1 = train_x[train_y == label1, :]
+    x0 = data[target == label0, :]
+    x1 = data[target == label1, :]
+    # 采用交叉验证法
+    for i in range(10):
+        test_x0 = x0[int(i * len(x0) / 10):int(i * len(x0) / 10 + len(x0) / 10)]
+        train_x0 = np.concatenate((x0[0:int(i * len(x0) / 10)], x0[int(i * len(x0) / 10 + len(x0) / 10):]), axis=0)
+        test_x1 = x1[int(i * len(x1) / 10):int(i * len(x1) / 10 + len(x1) / 10)]
+        train_x1 = np.concatenate((x1[0:int(i * len(x1) / 10)], x1[int(i * len(x1) / 10 + len(x1) / 10):]), axis=0)
+        # 数据标准化
+        scaler = preprocessing.StandardScaler().fit(np.concatenate((train_x0, train_x1),
+                                                                   axis=0))  # preprocessing.StandardScaler().fit()函数用于计算数据的均值和方差,np.concatenate()函数用于将两组数据合并
+        train_x0 = scaler.transform(train_x0)  # transform()函数用于将原数据替换为均值和方差
+        train_x1 = scaler.transform(train_x1)
+        test_x0 = scaler.transform(test_x0)
+        test_x1 = scaler.transform(test_x1)
+        # 训练模型
+        w, w0 = train(train_x0, train_x1)
 
-    test_x0 = test_x[test_y == label0, :]
-    test_x1 = test_x[test_y == label1, :]
+        # 测试模型
+        test_y0 = test(test_x0, w, w0)
+        test_y1 = test(test_x1, w, w0)
 
-# 数据标准化
-    scaler = preprocessing.StandardScaler().fit(np.concatenate((train_x0, train_x1), axis=0)) # preprocessing.StandardScaler().fit()函数用于计算数据的均值和方差,np.concatenate()函数用于将两组数据合并
-    train_x0 = scaler.transform(train_x0)#transform()函数用于将原数据替换为均值和方差
-    train_x1 = scaler.transform(train_x1)
-    test_x0 = scaler.transform(test_x0)
-    test_x1 = scaler.transform(test_x1)
-    return train_x0,train_x1,test_x0,test_x1
+        # 计算准确率
+        correct = np.sum(test_y0) + np.sum(1 - test_y1)
+        correct_rate += correct / (test_x0.shape[0] + test_x1.shape[0])
+    return correct_rate
 
-train_x0, train_x1,test_x0,test_x1=get_data()
-# 训练模型
-w, w0 = train(train_x0, train_x1)
 
-# 测试模型
-test_y0 = test(test_x0, w, w0)
-test_y1 = test(test_x1, w, w0)
-
-# 计算准确率
-correct = np.sum(test_y0) + np.sum(1 - test_y1)
-correct_rate = correct / (test_x0.shape[0] + test_x1.shape[0])
-print('Correct rate is: %.2f%%' % (correct_rate * 100))
+a = time.time()
+correct_rate = 0
+for i in range(10):
+    for j in range(i + 1, 10):
+        correct_rate += run(i, j)
+print('Correct rate is: %.2f%%' % (correct_rate * 10 / 45))
+print(time.time() - a)
