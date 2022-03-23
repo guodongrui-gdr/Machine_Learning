d5aca5ad5cd9b76a0763e71d16ce2cc261bd26b2
makabaka003 <3084608158@qq.com>
2022-03-24 02:33:16 +0800

    Linear Discriminant Analysis

--- Linear_Discriminant_Analysis/LDA_OvR.py
+++ Linear_Discriminant_Analysis/LDA_OvR.py
@@ -28,14 +28,16 @@ for label in range(10):
     x0 = data[target == label, :]  # 将一个类作为正例
     x1 = data[target != label, :]  # 将剩下的类作为反例
     # 数据标准化
-    scaler = preprocessing.StandardScaler().fit(np.concatenate((x0, x1), axis=0))  # preprocessing.StandardScaler().fit()函数用于计算数据的均值和方差,np.concatenate()函数用于将两组数据合并
+    scaler = preprocessing.StandardScaler().fit(np.concatenate((x0, x1),
+                                                               axis=0))  # preprocessing.StandardScaler().fit()函数用于计算数据的均值和方差,np.concatenate()函数用于将两组数据合并
     x0 = scaler.transform(x0)  # transform()函数用于将原数据替换为均值和方差
     x1 = scaler.transform(x1)
     # 训练模型
     w, w0 = train(x0, x1)
-    classifier.append((w, w0))
+    classifier.append((w, w0))  # 将训练好的模型参数存入数组中
 # 测试模型
-test_x = data[target == 0, :]
+label = 0  # 选取一类数字作为测试对象
+test_x = data[target == label, :]
 result = []
 for i in range(10):
     w = classifier[i][0]
@@ -44,9 +46,8 @@ for i in range(10):
 
     # 计算准确率
     correct_rate = 0
-
     correct = np.sum(test_y0)
     correct_rate += correct / test_x.shape[0]
     result.append((i, correct_rate))
-result.sort(key=lambda x: (x[1]), reverse=True)
-print("classify result is:", result[0][0])
+result.sort(key=lambda x: (x[1]), reverse=True)  # 将十个分类器得出的结果按准确率降序排序
+print("classify result is:", result[0][0])  # 最终结果为准确率最高的分类器对应的数字
---
+++ Linear_Discriminant_Analysis/LDA_crossvalidation.py
@@ -0,0 +1,63 @@
+import time
+import numpy as np
+from sklearn import preprocessing
+from sklearn.datasets import load_digits  # 导入数据
+
+
+def train(x0, x1):
+    m0 = x0.mean(0)
+    m1 = x1.mean(0)
+    Sw = np.dot((x0 - m0).T, (x0 - m0)) + np.dot((x1 - m1).T, (x1 - m1))
+    Sw = np.linalg.pinv(Sw)
+    w = Sw.dot(m0 - m1)
+    w0 = w.dot(-(m0 + m1) / 2)
+    return w, w0
+
+
+def test(x, w, w0):
+    y = x.dot(w)
+    flag = y - w0 > 0
+    return flag
+
+
+def run(label0, label1):
+    correct_rate = 0
+
+    digits = load_digits()
+    data = digits.data
+    target = digits.target
+
+    x0 = data[target == label0, :]
+    x1 = data[target == label1, :]
+    # 采用交叉验证法
+    for i in range(10):
+        test_x0 = x0[int(i * len(x0) / 10):int(i * len(x0) / 10 + len(x0) / 10)]
+        train_x0 = np.concatenate((x0[0:int(i * len(x0) / 10)], x0[int(i * len(x0) / 10 + len(x0) / 10):]), axis=0)
+        test_x1 = x1[int(i * len(x1) / 10):int(i * len(x1) / 10 + len(x1) / 10)]
+        train_x1 = np.concatenate((x1[0:int(i * len(x1) / 10)], x1[int(i * len(x1) / 10 + len(x1) / 10):]), axis=0)
+        # 数据标准化
+        scaler = preprocessing.StandardScaler().fit(np.concatenate((train_x0, train_x1),
+                                                                   axis=0))  # preprocessing.StandardScaler().fit()函数用于计算数据的均值和方差,np.concatenate()函数用于将两组数据合并
+        train_x0 = scaler.transform(train_x0)  # transform()函数用于将原数据替换为均值和方差
+        train_x1 = scaler.transform(train_x1)
+        test_x0 = scaler.transform(test_x0)
+        test_x1 = scaler.transform(test_x1)
+        # 训练模型
+        w, w0 = train(train_x0, train_x1)
+        # 测试模型
+        test_y0 = test(test_x0, w, w0)
+        test_y1 = test(test_x1, w, w0)
+        # 计算准确率
+        correct = np.sum(test_y0) + np.sum(1 - test_y1)
+        correct_rate += correct / (test_x0.shape[0] + test_x1.shape[0])
+    return correct_rate
+
+
+a = time.time()
+correct_rate = 0
+# 将十个数字两两组合分别训练
+for i in range(10):
+    for j in range(i + 1, 10):
+        correct_rate += run(i, j)
+print('Average correct rate is: {:.2f}%'.format(correct_rate * 10 / 45))
+print('Cost time is:{:.2f}s'.format(time.time() - a))
