8dd4945d4b9f0de17798a0acbdf9624ac3b285d4
makabaka003 <3084608158@qq.com>
2022-03-26 16:41:16 +0800

    md5

--- md5/md5.py
+++ md5/md5.py
@@ -1,5 +1,6 @@
 import hashlib
 import time
+from multiprocessing import Process
 
 
 def MD5(str1):
@@ -8,46 +9,40 @@ def MD5(str1):
     return hl.hexdigest()
 
 
-def run1(n):
-    with open('md5.txt','w') as f:
-        count = 0
+def run1(i):
+    with open('md5.txt', 'w') as f:
+        count = 15355554624
+        count0 = 1
         a = time.time()
-        time_count = 1
-        md5 = MD5(n)
-        f.write(md5+'\n')
-        for i in range(sum):
+        for j in range(i + count, i + count + int(k / threads)):
             count += 1
-            md5 = MD5(md5)
-
-            if n == md5:
+            b = str(hex(j))[2:]
+            md5 = MD5(b)
+            if b == md5:
                 print(md5)
                 break
-            if count % 100000 == 0:
-                print("progress:{:.2f}%,{:.4f}items/s".format(100*count/sum,100000 * time_count / (time.time() - a)))
-                f.write(md5 + '\n')
-                time_count += 1
-        print(md5)
+            if j % 100000 == 0:
+                print("{:.4f}items/s".format(100000 * threads * count0 / (time.time() - a)))
+                count0 += 1
+                f.write(str(count) + '\n')
         f.close()
-global sum
-sum=12*10**9
-a = time.time()
-run1('f57f2ea3634a0c4673f952d92b23f014')
-time=time.time()-a
-print("run time is:{:.2f}s".format(time))
-print("average speed is:{:.2f}items/s".format(sum/time))
-'''
 
 
+global threads, k
+threads = 10
+k = 10 ** 7
 if __name__ == '__main__':
+
     process_list = []
     j = []
     a = time.time()
     for i in range(threads):
-        j.append(16 ** 31 + 1 + int(i * 10 ** k / threads))
+        j.append(16 ** 31 + 1 + int(i * (16 ** 32 - 16 ** 31) / threads))
         p = Process(target=run1, args=(j[i],))
         p.start()
         process_list.append(p)
     for i in process_list:
         p.join()
-    print(time.time() - a)
-'''
+    t = time.time() - a
+    print('cost time is:{:.2f}'.format(t))
+    print('average speed is:{:.2f}items/s'.format(k / t))
